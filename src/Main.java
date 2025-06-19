d5e1677cf9f3c703dd16558d98b0a79e3a689054
Natali <natalievans95@gmail.com>
2025-06-19 15:20:01 +0300

    condition added

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ public class Main {
         // Пополнение 1
         int topUp = 2000; // пополнение на 2000
 
-        // считаем бонус как целое деление суммы пополнений на 100
-        int bonusMiles = topUp / 100;
+        int bonusMiles = 0;
 
-        int totalBalance = initialBalance + topUp;
+        // Условие для начисления бонусных миль
+        if (topUp >= 1000) {
+            bonusMiles = topUp / 100;
+        }
+
+        int totalBalance = initialBalance + topUp + bonusMiles;
 
         System.out.println("Бонусные мили: " + bonusMiles);
         System.out.println("Итоговый счет: " + totalBalance);
