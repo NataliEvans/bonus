954b44ea00b9f959341c27dde0ef835dd343b58c
Natali <natalievans95@gmail.com>
2025-06-19 14:07:20 +0300

    cheange commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // Начальный баланс клиента
         int initialBalance = 50;
-        int topUp1 = 200;   // пополнение 1
-        int topUp2 = 2000;  // пополнение 2
+
+        // Пополнение 1
+        int topUp = 2000; // пополнение на 2000
 
         // считаем бонус как целое деление суммы пополнений на 100
-        int bonusMiles = (topUp1 + topUp2) / 100;
+        int bonusMiles = topUp / 100;
 
-        int totalBalance = initialBalance + topUp1 + topUp2;
+        int totalBalance = initialBalance + topUp;
 
         System.out.println("Бонусные мили: " + bonusMiles);
         System.out.println("Итоговый счет: " + totalBalance);
